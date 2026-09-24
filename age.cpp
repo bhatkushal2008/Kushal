a0bd321bc1252e68247ba94c32013577cd887589
bhatkushal2008 <bhatkushal72@gmail.com>
2026-09-24 14:06:54 +0000

    hello

---
+++ age.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int age;
+    cout<<"Enter your Age: ";
+    cin>>age;
+    cout<<"Your Age: "<<age;
+    return 0;
+}
---
+++ student.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+
+    int roll_no;
+    float marks;
+    string name;
+
+    cout<<"Enter your roll no: ";
+    cin>>roll_no;
+
+    cout<<"Enter your marks: ";
+    cin>>marks;
+
+    cout<<"Enter your name: ";
+    cin>>name;
+
+    cout<<"\nName: "<<name;
+    cout<<"\nroll no: "<<roll_no;
+    cout<<"\nmarks: "<<marks;
+
+    return 0;
+}
